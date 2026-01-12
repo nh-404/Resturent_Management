b3f09227da1778c2785a230b442e2bfbf4fd8f07
nh-404 <nh404@proton.me>
2026-01-12 23:45:09 +0600

    increase reset pass security

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from django.template.loader import render_to_string
 from accounts.models import User
 from django.urls import reverse
@@ -21,29 +24,51 @@ def signUp(request):
         email = request.POST.get('email')   
         password = request.POST.get('password') 
 
-        #chexking data from db
-        if email and password and full_name and phone:
 
-            user = User.objects.create_user(
+        #chexking data from db
+        if not all([full_name, phone, email, password]):
+            messages.error(request, 'All fields are requried')
+            return redirect('signUp')
+
+
+        #duplicate email checking
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email already existed!")
+            return redirect('signUp')
+
+
+        #password validation
+        try:
+            validate_password(password)
+
+        except ValidationError as e:
+
+            for error in e.messages:
+                messages.error(request, error)
+            return redirect('signUp')
+
+
+        #Create User
+        user = User.objects.create_user(
                 email=email,
                 password=password,
                 full_name=full_name,
                 phone=phone,
             )
 
-            login(request, user)
-            return redirect('home')
-
-        else:
-            
-            return redirect('signUP')
-
+        #user validation
+        login(request, user)
+        messages.success(request, 'Account created successfully')
+        return redirect('home')
 
     return render(request, 'auth/signUp.html')
 
 
 
-#_____Login___
+
+
+
+#_____Login___nh404@duxk.com
 def signIn(request):
 
     if request.method == 'POST':
@@ -51,15 +76,22 @@ def signIn(request):
         email = request.POST.get('email')   
         password = request.POST.get('password') 
 
+
+        #validate email and password
+        if not email or not password:
+            messages.error(request, "Email and Password required!")
+            return redirect('login')
+
         user = authenticate(request, email=email, password=password)
 
-        if user is not None:
+
+        #Validate Login
+        if user:
             login(request, user)
             return redirect('home')
-
         else:
-            return render(request, 'auth/login.html', {'error' : 'Invalid password or email'})
-
+            messages.error(request, "invalid Email or Password!")
+            return redirect('login')
 
     return render(request, 'auth/login.html')
 
@@ -70,7 +102,8 @@ def signIn(request):
 def signOut(request):
 
     logout(request)
-    return redirect('login')
+    messages.success(request, 'Logout successfully!')
+    return redirect('home')
 
 
 
@@ -79,48 +112,49 @@ def signOut(request):
 def reset_password(request):
 
     if request.method == 'POST':
+
         email = request.POST.get('email')
 
-        try:
-            user = User.objects.get(email=email)
+        user = User.objects.filter(email=email).first()
 
+        if user:
             uid = urlsafe_base64_encode(force_bytes(user.pk))
             token = default_token_generator.make_token(user)
 
             reset_link = request.build_absolute_uri(
-                reverse('reset_password_confirm', kwargs={'uidb64': uid, 'token': token})
+                reverse(
+                    'reset_password_confirm',
+                    kwargs={'uidb64': uid, 'token': token}
+                )
             )
 
+            html_content = render_to_string(
+                'auth/reset_password/reset_email_template.html',
+                {'reset_link': reset_link}
+            )
 
-
-            # Render HTML template
-            html_content = render_to_string('auth/reset_password//reset_email_template.html', {'reset_link': reset_link})
-
-            # Optional: plain text fallback
             text_content = f"""
-            Hello,
+                Hello,
 
-            You requested a password reset. Click the link below to reset your password:
+                You requested a password reset. Click the link below to reset your password:
 
-            {reset_link}
+                {reset_link}
 
-            If you did not request this, please ignore this email.
-            """
+                If you did not request this, please ignore this email.
+                """
+                
+            email_message = EmailMultiAlternatives(
 
-            # Send email
-            msg = EmailMultiAlternatives(
-                subject="Password Reset Request",
+                subject='Request for password reset',
                 body=text_content,
                 from_email=settings.DEFAULT_FROM_EMAIL,
                 to=[email],
             )
-            msg.attach_alternative(html_content, "text/html")
-            msg.send()
-            
 
-        except User.DoesNotExist:
-            pass
+            email_message.attach_alternative(html_content, 'text/html')
+            email_message.send()
 
+        # Always show success page (prevents email enumeration)
         return render(request,'auth/reset_password/reset_password_sent.html')
 
     return render(request,'auth/reset_password/reset_password.html')
@@ -128,41 +162,54 @@ def reset_password(request):
 
 
 
-#___________reset_password_confirm__________
+
+
+# #___________reset_password_confirm__________
 def reset_password_confirm(request, uidb64, token):
 
+    # Decode the user
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
-
     except (User.DoesNotExist, ValueError, TypeError):
         user = None
 
-#__User Condition checking___
-    if user is None or not default_token_generator.check_token(user, token):
+    # Validate token
+    if not user or not default_token_generator.check_token(user, token):
         return render(
             request,
             'auth/reset_password/reset_password_invalid.html'
         )
 
-#__password Condition checking for template___
+    errors = []  # Collect validation errors
+
     if request.method == 'POST':
 
         password = request.POST.get('password')
         confirm_password = request.POST.get('confirm_password')
 
-
+        # Required fields
         if not password or not confirm_password:
-            return render(request, 'auth/reset_password/reset_password_confirm.html', {'error': 'All fields are required'})
+            errors.append('All fields are required.')
 
-        if password != confirm_password:
-            return render(request, 'auth/reset_password/reset_password_confirm.html',{'error': 'Passwords do not match'})
+        # Password match
+        elif password != confirm_password:
+            errors.append('Passwords do not match.')
 
-#__Reset hash password______
-        user.set_password(password)
-        user.save()
+        # Django password validation
+        else:
+            try:
+                validate_password(password, user)
+            except ValidationError as e:
+                errors.extend(e.messages)
 
-        return redirect('login')
+        # If no errors, set password
+        if not errors:
+            user.set_password(password)
+            user.save()
+            return redirect('login')
+
+    # Render template with errors if any
+    return render(request,'auth/reset_password/reset_password_confirm.html',{'errors': errors, 'uidb64': uidb64, 'token': token})
 
 
-    return render(request, 'auth/reset_password/reset_password_confirm.html')
